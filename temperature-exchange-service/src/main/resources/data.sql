1cdea206a2f82cd4e8d5283e4168e0dc810c4b2c
Dipti <pangam.dipti.99@gmail.com>
2021-05-27 12:45:20 +0530

    change in temperature conversion value

--- temperature-exchange-service/src/main/resources/data.sql
+++ temperature-exchange-service/src/main/resources/data.sql
@@ -4,4 +4,4 @@ values(10001,'C','F',1.8,32,'');
 
 insert into temperature_exchange
 (id, temperature_from, temperature_to, conversion_multiple, conversion_add_sub, environment)
-values(10002,'F','C',0.5555,-32,'');
+values(10002,'F','C',0.55,-17.222,'');
